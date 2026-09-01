ec84fbddcd0448f1f29175a0f3cd4877a92f6268
Abhishek <office-user@://github.com>
2026-09-01 16:19:16 +0530

    1/9 prod

--- src/index.ts
+++ src/index.ts
@@ -32,17 +32,12 @@ const PORT = process.env.PORT;
 app.use(
   cors({
     // local
-    origin: ["http://localhost:3000", "http://localhost:3001"],
-    // staging
-    // origin: [
-    //    'https://tirupatitravel.in/',
-    //    'https://cms.tirupatitravel.in/',
-    // ],
+    // origin: ["http://localhost:3000", "http://localhost:3001"],
     // production
-    // origin: [
-    //     'https://tirupatitravel.in/',
-    //    'https://cms.tirupatitravel.in/',
-    // ],
+    origin: [
+        'https://tirupatitravel.in/',
+       'https://cms.tirupatitravel.in/',
+    ],
 
     credentials: true, // Allow credentials to be sent
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -52,12 +47,12 @@ app.use(
 const io = new Server(server, {
   cors: {
     // local
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    // origin: ["http://localhost:3000", "http://localhost:3001"],
     // production
-    // origin: [
-    //       'https://tirupatitravel.in/',
-    //    'https://cms.tirupatitravel.in/',
-    // ],
+    origin: [
+          'https://tirupatitravel.in/',
+       'https://cms.tirupatitravel.in/',
+    ],
 
     methods: ["GET", "POST"],
     credentials: true,
